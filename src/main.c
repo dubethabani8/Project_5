441da90f05135962cbe399058681bb39bcf393bd
Thabani Dube <tdube2@u.rochester.edu>
2019-04-29 10:11:53 -0400

    Circuit_a completed

--- src/main.c
+++ src/main.c
@@ -39,6 +39,42 @@ static Circuit* and3_Circuit() {
 	return circuit;
 }
 
+static Circuit* Circuit_a() {
+	Boolean* x = new_Boolean(false);
+	Boolean* y = new_Boolean(false);
+	Boolean* z = new_Boolean(false);
+	Boolean** inputs = new_Boolean_array(3);
+	inputs[0] = x;
+	inputs[1] = y;
+	inputs[2] = z;
+
+	Boolean* out = new_Boolean(false);
+	Boolean** outputs = new_Boolean_array(1);
+	outputs[0] = out;
+
+	Gate* A1 = new_Inverter();
+	Gate* A2 = new_AndGate();
+	Gate* A3 = new_AndGate();
+	Gate* A4 = new_OrGate();
+	Gate** gates = new_Gate_array(4);
+	gates[0] = A1;
+	gates[1] = A2;
+	gates[2] = A3;
+	gates[3] = A4;
+
+	Circuit *circuit = new_Circuit(3, inputs, 1, outputs, 4, gates);
+	Circuit_connect(circuit, x, Gate_getInput(A2, 0));
+	Circuit_connect(circuit, y, Gate_getInput(A1, 0));
+	Circuit_connect(circuit, y, Gate_getInput(A3, 0));
+	Circuit_connect(circuit, z, Gate_getInput(A3, 1));
+	Circuit_connect(circuit, Gate_getOutput(A1), Gate_getInput(A2, 1));
+	Circuit_connect(circuit, Gate_getOutput(A2), Gate_getInput(A4, 0));
+	Circuit_connect(circuit, Gate_getOutput(A3), Gate_getInput(A4, 1));
+	Circuit_connect(circuit, Gate_getOutput(A4), out);
+
+	return circuit;
+}
+
 static char* b2s(bool b) {
 	return b ? "T" : "F";
 }
@@ -54,7 +90,7 @@ static void test3In1Out(Circuit* circuit, bool in0, bool in1, bool in2) {
 
 
 int main(int argc, char **argv) {
-	Circuit* circuit = and3_Circuit();
+	Circuit* circuit = Circuit_a();
 	printf("The and3 circuit (AND of three inputs):\n");
 	Circuit_dump(circuit);
 	printf("\n");
